58465b0fee890939c276150d59d782e7125adebe
Corey Roberts <corey.roberts@mapmyfitness.com>
2012-07-12 23:37:11 -0500

    Changed boilerplate.

--- HandsOfTimeSolver/main.m
+++ HandsOfTimeSolver/main.m
@@ -3,7 +3,7 @@
 //  HandsOfTimeSolver
 //
 //  Created by Corey Roberts on 2/21/12.
-//  Copyright (c) 2012 University of Texas at Austin. All rights reserved.
+//  Copyright (c) 2012 SpacePyro Productions. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- HandsOfTimeSolverTests/HandsOfTimeSolverTests.h
+++ HandsOfTimeSolverTests/HandsOfTimeSolverTests.h
@@ -3,7 +3,7 @@
 //  HandsOfTimeSolverTests
 //
 //  Created by Corey Roberts on 2/21/12.
-//  Copyright (c) 2012 University of Texas at Austin. All rights reserved.
+//  Copyright (c) 2012 SpacePyro Productions. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
